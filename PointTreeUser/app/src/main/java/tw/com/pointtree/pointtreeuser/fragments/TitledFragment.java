7096e9f9c536e53d1c2c314ebfd0aad2f5e9dac2
Stanley Ding <stanleyding.tw@gmail.com>
2016-04-20 15:27:30 +0800

    Modify TitledFragment.java.
    - Make TitledFragment abstract.
    - Add abstract method `getFragmentTitle` to get the fragment title.
    - Change some method signatures.

--- PointTreeUser/app/src/main/java/tw/com/pointtree/pointtreeuser/fragments/TitledFragment.java
+++ PointTreeUser/app/src/main/java/tw/com/pointtree/pointtreeuser/fragments/TitledFragment.java
@@ -1,6 +1,7 @@
 package tw.com.pointtree.pointtreeuser.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,39 +11,33 @@ import android.widget.TextView;
 
 import tw.com.pointtree.pointtreeuser.R;
 
-public class TitledFragment extends Fragment {
-    private static final String PARAM_FRAGMENT_TITLE = "fragmentTitle";
+public abstract class TitledFragment extends Fragment {
     private String fragmentTitle;
+
+    /**
+     * Subclass should return the title of this fragment by this method.
+     * @return The title of the fragment.
+     */
+    @NonNull public abstract String getFragmentTitle();
+
+    /**
+     * Called to set the content view below the title.
+     * @param inflater A layout inflater.
+     * @param container The parent view to hold the view. The view should not add itself to the
+     *                  parent.
+     * @return The content view.
+     */
+    @NonNull public abstract View onCreateContentView(LayoutInflater inflater,
+                                                      ViewGroup container);
 
     public TitledFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     * @param fragmentTitle The title to display for this fragment.
-     * @return A new instance of fragment TitledFragment.
-     */
-    public static TitledFragment newInstance(String fragmentTitle) {
-        TitledFragment fragment = new TitledFragment();
-        Bundle args = new Bundle();
-        args.putString(PARAM_FRAGMENT_TITLE, fragmentTitle);
-        fragment.setArguments(args);
-        return fragment;
+        this.fragmentTitle = getFragmentTitle();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            fragmentTitle = getArguments().getString(PARAM_FRAGMENT_TITLE);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    final public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                   Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_titled, container, false);
 
         TextView fragmentTitleView = (TextView) view.findViewById(R.id.fragment_title);
@@ -50,13 +45,7 @@ public class TitledFragment extends Fragment {
 
         FrameLayout contentContainer = (FrameLayout) view.findViewById(R.id.content_container);
         View contentView = onCreateContentView(inflater, contentContainer);
-        if (contentView != null) {
-            contentContainer.addView(contentView);
-        }
+        contentContainer.addView(contentView);
         return view;
-    }
-
-    public View onCreateContentView(LayoutInflater inflater, ViewGroup container) {
-        return null;
     }
 }
