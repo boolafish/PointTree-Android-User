8e1b987aca8d082c7d20003e3f34531f0b925e9c
TSAN-HSIANG HSU <bun919tw@gmail.com>
2016-07-11 20:53:33 +0800

    Store info fragment UI (#15)

--- PointTreeUser/app/src/main/java/tw/com/pointtree/pointtreeuser/fragments/StoreInfoFragment.java
+++ PointTreeUser/app/src/main/java/tw/com/pointtree/pointtreeuser/fragments/StoreInfoFragment.java
@@ -1,10 +1,13 @@
 package tw.com.pointtree.pointtreeuser.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import tw.com.pointtree.pointtreeuser.R;
 
@@ -31,5 +34,26 @@ public class StoreInfoFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_store_info, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        addStorePreference(view);
+    }
+
+    private void addStorePreference(View view) {
+        // TODO: should use real data in the future
+        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.last_content_layout);
+
+        TextView storePreference = new TextView(getActivity());
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(15, 0, 0, 0);
+        storePreference.setLayoutParams(layoutParams);
+        storePreference.setBackgroundResource(R.drawable.store_preference);
+        storePreference.setText("滿200可外送");
+
+        linearLayout.addView(storePreference);
     }
 }
